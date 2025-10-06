5333b30a50427f0715eb1c3a268b7cde5ac5f91a
paulo <paulojosueb58@gmail.com>
2025-10-06 10:39:13 -0600

    Primer commit: Estructura inicial del proyecto

---
+++ backend-cartapp/src/main/java/com/paulo/backend/cartapp/backend_cartapp/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.paulo.backend.cartapp.backend_cartapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.paulo.backend.cartapp.backend_cartapp.models.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    
+} 
---
+++ backend-cartapp/src/main/java/com/paulo/backend/cartapp/backend_cartapp/services/ProductService.java
@@ -0,0 +1,10 @@
+package com.paulo.backend.cartapp.backend_cartapp.services;
+
+import java.util.List;
+
+import com.paulo.backend.cartapp.backend_cartapp.models.entities.Product;
+
+public interface ProductService {
+    List<Product> findAll();
+
+}
